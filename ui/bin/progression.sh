05d167275e5b1c3d15db4b77e0238762e1a617dd
Nicolas Pierson <nicolas.pierson@cornerside.fr>
2020-10-03 08:52:39 +0700

    feat: update progression bar

--- ui/bin/progression.sh
+++ ui/bin/progression.sh
@@ -30,5 +30,4 @@ oneDay=`python -c "print('{:0.4f}'.format((3600*24)*100/(365*24*3600)))"`
 
 #echo "<fc=#5E8D87>$life</fc>% tg <fc=#CD5C5C>40y</fc> • <fc=#5E8D87>$oneYear</fc>% for <fc=#F0C674>1y</fc> • <fc=#B5BD68>$ratio</fc>% tg <fc=#CD5C5C>1y</fc> • <fc=#B5BD68>$oneDay</fc>% for <fc=#F0C674>1d</fc> $(ProgressBar $(($(date +"%H")-6)) 12)"
 
-echo " $(ProgressBar $(($(date +"%H")-6)) 12) • <fc=#5E8D87>$life</fc>% tg <fc=#CD5C5C>40y</fc> • <fc=#B5BD68>$ratio</fc>(+$oneDay)% tg <fc=#CD5C5C>1y</fc>"
-
+echo " $(ProgressBar $(($(date +"%k")-6)) 12) • <fc=#5E8D87>$life</fc>% tg <fc=#CD5C5C>40y</fc> • <fc=#B5BD68>$ratio</fc>(+$oneDay)% tg <fc=#CD5C5C>1y</fc>"
