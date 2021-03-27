53d353c6c524bb007be9569c5fdb4e49448ade96
Nicolas Pierson <nicolas.pierson@cornerside.fr>
2021-03-27 10:06:08 +0700

    feat: add ip location

--- ui/bin/xmobarrc_ip.sh
+++ ui/bin/xmobarrc_ip.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo "`curl -s ifconfig.me | awk -F '.' '{print $1"."$2"."$3".xxx"}'`"
+cloudflare=`curl -s 'https://www.cloudflare.com/cdn-cgi/trace'`
+
+ip=`echo $cloudflare | awk -F"[ =]" '{printf "%s",$6}'`
+filteredIp=`echo $ip | awk -F '.' '{print $1"."$2"."$3".xxx"}'`
+loc=`echo $cloudflare | awk -F"[ =]" '{printf "%s",$18}'`
+
+echo $filteredIp $loc
