57fe7202d24069cd95e7a2332f2bca79d9b2e52f
Nicolas Pierson <nicolas.pierson@cornerside.fr>
2021-08-22 14:36:42 +0700

    feat: parse IPv6 xmobar

--- ui/bin/xmobarrc_ip.sh
+++ ui/bin/xmobarrc_ip.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 cloudflare=`curl -s 'https://www.cloudflare.com/cdn-cgi/trace'`
-
 ip=`echo $cloudflare | awk -F"[ =]" '{printf "%s",$6}'`
-filteredIp=`echo $ip | awk -F '.' '{print $1"."$2"."$3".xxx"}'`
-loc=`echo $cloudflare | awk -F"[ =]" '{printf "%s",$18}'`
 
-network=''
-
-if [[ $ip == 159.* ]]; then
-  network='<fc=#94A65D>PR</fc>'
+if [[ $ip == *"."* ]]; then
+  filteredIp=`echo $ip | awk -F '.' '{print $1"."$2"."$3".xxx"}'`
 fi
 
-echo $filteredIp $loc $network
+if [[ $ip == *":"* ]]; then
+  filteredIp=`echo $ip | awk -F ':' '{print $1":"$2":::::"$7":"$8}'`
+fi
+
+location=`echo $cloudflare | awk -F"[ =]" '{printf "%s",$18}'`
+
+echo $filteredIp $location
 
