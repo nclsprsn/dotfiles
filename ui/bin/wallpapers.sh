bbc9e6e90c77b0c687965ba54ff13e450588f8aa
Nicolas Pierson <nicolas.pierson@cornerside.fr>
2022-02-12 09:18:44 +0700

    feat(background): disable backround cron

--- ui/bin/wallpapers.sh
+++ ui/bin/wallpapers.sh
@@ -9,6 +9,7 @@ DURATION="15m"
 while true; do
 	find "$1" -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' -o -name '*.jpe' \) -print0 |
 		shuf -n1 -z | xargs -0 feh --bg-center
+	break
 	sleep $DURATION
 done
 
