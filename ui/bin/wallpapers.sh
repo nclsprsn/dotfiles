a21fbe58e72a2a2025fe822cf2e7123f8a31390f
Nicolas Pierson <nicolas.pierson@cornerside.fr>
2022-09-25 13:00:17 +0700

    feat: switch to AMD cpu

--- ui/bin/wallpapers.sh
+++ ui/bin/wallpapers.sh
@@ -8,7 +8,7 @@ DURATION="15m"
 
 while true; do
 	find "$1" -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' -o -name '*.jpe' \) -print0 |
-		shuf -n1 -z | xargs -0 feh --bg-center
+		shuf -n1 -z | xargs -0 feh --bg-fill
 	break
 	sleep $DURATION
 done
