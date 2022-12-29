345f5c3c3301392c43463fa581a27dce08cbca0c
Nicolas Pierson <nicolas.pierson@cornerside.fr>
2022-12-29 10:51:07 +0700

    feat(xmobar): display full ip address

--- ui/bin/xmobarrc_ip.sh
+++ ui/bin/xmobarrc_ip.sh
@@ -4,7 +4,8 @@ cloudflare=`curl -s 'https://www.cloudflare.com/cdn-cgi/trace'`
 ip=`echo $cloudflare | awk -F"[ =]" '{printf "%s",$6}'`
 
 if [[ $ip == *"."* ]]; then
-  filteredIp=`echo $ip | awk -F '.' '{print $1"."$2"."$3".xxx"}'`
+  #filteredIp=`echo $ip | awk -F '.' '{print $1"."$2"."$3".xxx"}'`
+  filteredIp=`echo $ip | awk -F '.' '{print $1"."$2"."$3"."$4}'`
 fi
 
 if [[ $ip == *":"* ]]; then
